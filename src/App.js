f6dabfc3c12b52c60cdc010587a08972845e527b
hsjung22 <hsjung22@gmail.com>
2017-08-06 15:37:34 -0700

    feat: Using react-redux connect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { addRecipe, removeFromCalendar } from './actions'
 
 class App extends Component {
 
   render() {
+    console.log("Props", this.props)
+
     return (
       <div>
         Hello World
@@ -11,4 +15,27 @@ class App extends Component {
   }
 }
 
-export default App
+function mapStateToProps(calendar) {
+  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
+  return {
+    calendar: dayOrder.map(day => ({
+      day,
+      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = calendar[day][meal]
+          ? calendar[day][meal]
+          : null
+        return meals
+      }, {})
+    }))
+  }
+}
+
+function mapDispatchToProps (dispatch) {
+  return {
+    selectRecipe: (data) => dispatch(addRecipe(data)),
+    remove: (data) => dispatch(removeFromCalendar(data))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
